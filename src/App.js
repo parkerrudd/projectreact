bf587d89bf7be055b6b2a6896399b6ff9612ebd5
Parker Rudd <parkerbrudd@gmail.com>
2022-03-25 13:20:57 -0600

    made a bunch of tweets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tweet from './tweet.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return(
+    <div className="app">
+      <Tweet name='Parker' message='The Broncos are a wagon' likes='3'/>
+      <Tweet name='Jon Snow' message='I am a bastard' likes='69'/>
+      <Tweet name='Bran Stark' message='I am useless' likes='700K'/>
+      <Tweet name='Mosh' message='New course out now!' likes='121'/>
     </div>
-  );
+  ); 
 }
 
-export default App;
+export default App; 
