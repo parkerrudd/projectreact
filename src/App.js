df47787d40b45d722811a98042826c523a26ddd9
Parker Rudd <parkerbrudd@gmail.com>
2022-03-27 11:39:03 -0600

    added onClick events to button and toggled className

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Tweet from './tweet.js'
 
 function App() {
 
+  const [isRed, setRed] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+    setRed(!isRed); 
+  }
+
   return(
     <div className="app">
+      <button onClick={increment}>Increment</button>
+      <h1 className={isRed ? 'red' : ''}>Change my color</h1>
+      <h1>{count}</h1>
       <Tweet name='Parker' message='The Broncos are a wagon' likes='3'/>
       <Tweet name='Jon Snow' message='I am a bastard' likes='69'/>
       <Tweet name='Bran Stark' message='I am useless' likes='700K'/>
